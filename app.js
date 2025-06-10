859f9f9751a95b40ef1e1fb54a0035af3d5c0124
g4mless <g4mless@proton.me>
2025-06-10 15:23:14 +0700

    update

--- app.js
+++ app.js
@@ -5,33 +5,33 @@ const port = 5500
 const baseURL = 'https://dgolbn4rrbixbcll.public.blob.vercel-storage.com';
 
 const files = [
-    'keqing-bouquet.webp',
-    'keqing-looktoviewer2.webp',
-    'keqing-lovesmile.webp',
-    'keqing-casual.webp',
-    'keqing-portrait.webp',
-    'keqing-landscape.webp',
-    'keqing-oneeyeclosed.webp',
-    'keqing-looktoviewer.webp',
-    'keqing-blushingcasual.webp',
-    'keqing-looktoviewer.webp',
-    'keqing-holdingsword.webp',
-    'keqing-gloom.webp',
-    'keqing-brush.webp',
-    'keqing-umbrella.webp',
-    'keqing-headrest.webp',
-    'keqing-kimono.webp',
-    'keqing-sit.webp',
-    'keqing-see1.webp',
-    'keqing-see2.webp',
-    'keqing-ptrt2.webp',
-    'keqing-blinking.webp',
-    'keqing-ptrt1.webp',
-    'keqing-aura.webp',
-    'keqing-wet.webp',
-    'keqing-wp1.webp',
-    'keqing-wp2.webp',
-    'keqing-baloon.webp'
+    {filename:'keqing-bouquet.webp', source: 'https://www.pixiv.net/en/artworks/124469294'},
+    {filename:'keqing-looktoviewer2.webp', source: 'https://x.com/fal_maro/status/1835272406103121967'},
+    {filename:'keqing-lovesmile.webp', source: 'https://x.com/tyenka7728/status/1625419589508124674'},
+    {filename:'keqing-casual.webp', source: 'https://www.zerochan.net/3555637'},
+    {filename:'keqing-portrait.webp', source: 'https://www.pixiv.net/artworks/97767520'},
+    {filename:'keqing-landscape.webp', source: 'https://danbooru.donmai.us/posts/4183543'},
+    {filename:'keqing-oneeyeclosed.webp', source: 'https://www.pixiv.net/artworks/99030313'},
+    {filename:'keqing-looktoviewer.webp', source: 'https://x.com/ringozaka_0913/status/1812817203080974838'},
+    {filename:'keqing-blushingcasual.webp', source: 'https://x.com/tozi526/status/1858888140146254273'},
+    {filename:'keqing-holdingsword.webp', source:'https://danbooru.donmai.us/posts/5992435'},
+    {filename:'keqing-gloom.webp', source: 'https://www.pixiv.net/en/artworks/86046891'},
+    {filename:'keqing-brush.webp', source: 'https://x.com/redi_rkgk/status/1328877046412537858'},
+    {filename:'keqing-umbrella.webp', source: 'https://www.pixiv.net/en/artworks/118174964'},
+    {filename:'keqing-umbrella2.webp', source: 'https://danbooru.donmai.us/posts/7496493'},
+    {filename:'keqing-headrest.webp', source: 'https://x.com/_luna610/status/1706959678205579453'},
+    {filename:'keqing-kimono.webp', source: 'https://safebooru.org/index.php?page=post&s=view&id=5506434'},
+    {filename:'keqing-sit.webp', source:'https://www.pixiv.net/en/artworks/106623085'},
+    {filename:'keqing-see1.webp', source: 'https://www.pixiv.net/en/artworks/102934427'},
+    {filename:'keqing-see2.webp', source: 'https://www.pixiv.net/en/artworks/102934427'},
+    {filename:'keqing-ptrt2.webp', source: 'https://www.pixiv.net/artworks/109266324'},
+    {filename:'keqing-blinking.webp', source: 'https://x.com/ebibi_chiriri/status/1578037551717580800'},
+    {filename:'keqing-ptrt1.webp', source: 'https://www.pixiv.net/en/artworks/126377588'},
+    {filename:'keqing-aura.webp', source: 'https://www.pixiv.net/en/artworks/130066464'},
+    {filename:'keqing-wet.webp', source: 'https://x.com/mar_R_o/status/1727111162284986484'},
+    {filename:'keqing-wp1.webp', source: 'https://www.pixiv.net/en/artworks/87977897'},
+    {filename:'keqing-wp2.webp', source: 'https://www.pixiv.net/en/artworks/90015173'},
+    {filename:'keqing-baloon.webp', source: 'https://x.com/aoirooto/status/1768200404641153087'}
 ];
 
 app.get("/", (req, res) => {
@@ -39,9 +39,9 @@ app.get("/", (req, res) => {
 })
 
 app.get("/keqing", (req, res) => {
-    const result = files.map(name => ({
-        name,
-        url: `${baseURL}/${name}`
+    const result = files.map(file => ({
+        url: `${baseURL}/${file.filename}`,
+        source: file.source
     }));
     res.json(result)
 });
