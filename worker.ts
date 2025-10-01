fdd98c004321ab236961d9cee3c89998e6db1f33
g4mless <g4mless@proton.me>
2025-10-01 23:12:54 +0700

    forgot cors lol

--- worker.ts
+++ worker.ts
@@ -1,7 +1,10 @@
 import { Hono } from 'hono/tiny';
 import apiApp from './api/index';
+import { cors } from 'hono/cors';
 
 const worker = new Hono();
+
+worker.use(cors());
 
 // Root redirect to /api
 worker.get('/', (c) => c.redirect('/api', 302));
