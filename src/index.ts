ee49c191e903c92d6e28a292a12dc72d05421699
g4mless <g4mless@proton.me>
2025-11-17 20:36:53 +0700

    migrate to elysia, cuz why not?

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,29 @@
-import { Elysia } from "elysia";
+import { Elysia } from "elysia"
+import { cors } from '@elysiajs/cors'
+import imagesJson from '../index.json'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+type KeqingImage = { filename: string; source: string }
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+const files: KeqingImage[] = imagesJson as KeqingImage[]
+const baseURL = 'https://rrddcemyrcmrmpjnysgb.supabase.co/storage/v1/object/public/keq/keqing-'
+
+const app = new Elysia().use(cors()).listen(3000)
+  .get("/", () => "Keqing API")
+
+  .get('/all', () => {
+    const result = [...files].reverse().map((file) => ({
+      url: `${baseURL}${file.filename}`,
+      source: file.source
+    }))
+    return { result }
+  })
+  
+  .get('/gacha', () => {
+    const random = files[Math.floor(Math.random() * files.length)]
+    return {
+      url: `${baseURL}${random.filename}`,
+      source: random.source
+    }
+  })
+
+console.log(`running at ${app.server?.hostname}:${app.server?.port}`);
